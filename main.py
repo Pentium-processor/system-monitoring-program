4845d751095f9917692c5ba411431c63085cbeab
Sys Acrunis 1o1 <114465675+Pentium-processor@users.noreply.github.com>
2025-08-08 22:14:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ if __name__=="__main__":
    table.add_column("[green]MEMORY[/]",style="green on black")
    table.add_column("[green]PIDS[/]",style="green on black")
    table.add_row(f'0:{psutil.cpu_percent(interval=0.1,percpu=True)[0]}%',f'{psutil.cpu_freq(percpu=True)[0][0]} MHz',f'USED:{psutil.virtual_memory()[2]}%')
-   table.add_row(f'1:{psutil.cpu_percent(interval=0.1,percpu=True)[1]}%',f'{psutil.cpu_freq(percpu=True)[1][0]} MHz',f'AVAIL:{round(psutil.virtual_memory()[1]/1e+9,2)} GB',f'{f"\n".join([x for x in mapped])}')
+   table.add_row(f'1:{psutil.cpu_percent(interval=0.1,percpu=True)[1]}%',f'{psutil.cpu_freq(percpu=True)[1][0]} MHz',f'AVAIL:{round(psutil.virtual_memory()[1]/1e+9,2)} GB',f'{"\n".join([x for x in mapped])}')
    time.sleep(1)
    subprocess.call("clear")
    console.print(table,end="\r")
